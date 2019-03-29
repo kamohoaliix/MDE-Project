4e6e646558e89cd0e1feec6c88869ded807b9d8f
kamohoaliix <joshuaboddy1@gmail.com>
2019-03-29 16:25:36 +0000

    Added the P5 Project

--- src/pages/Data.js
+++ src/pages/Data.js
@@ -2,82 +2,10 @@ import React, { Component } from 'react';
 import '../App.css';
 
 class Data extends Component {
-  state = {
-    physical: false,
-    environmental: false,
-    social: false
-  }
-
-  constructor(props) {
-    super(props);
-    this.togglePhysical = this.togglePhysical.bind(this);
-    this.toggleEnvironmental = this.toggleEnvironmental.bind(this);
-    this.toggleSocial = this.toggleSocial.bind(this);
-  }
-
-  togglePhysical() {
-    this.setState({
-      physical: !this.state.physical
-    })
-  }
-
-  toggleEnvironmental() {
-    this.setState({
-      environmental: !this.state.environmental
-    })
-  }
-
-  toggleSocial() {
-    this.setState({
-      social: !this.state.social
-    })
-  }
-
   render() {
     return (
       <div className="Data">
-        <table>
-          <tr>
-            <td valign="top">
-              <span onClick={this.togglePhysical}>Physical</span><br />
-              { this.state.physical &&
-                <ul>
-                  <li>Geophysical</li>
-                  <li>Sedimentary</li>
-                  <li>Geotechnical</li>
-                </ul>
-              }
-            </td>
-            <td valign="top">
-              <span onClick={this.toggleEnvironmental}>Environmental</span><br />
-              { this.state.environmental &&
-                <ul>
-                  <li>Meteorology</li>
-                  <li>Coastal Processes</li>
-                  <li>Benthic Ecology</li>
-                  <li>Ornathology and Marine Mammals</li>
-                  <li>Epifauna and Fish</li>
-                  <li>Metocean</li>
-                  <li>Intertidal Ecology</li>
-                  <li>Onshore</li>
-                  <li>Noise</li>
-                </ul>
-              }
-            </td>
-            <td valign="top">
-              <span onClick={this.toggleSocial}>Social</span><br />
-              { this.state.social &&
-                <ul>
-                  <li>Environmental Statements</li>
-                  <li>Archeology</li>
-                  <li>Shipping and Navigation</li>
-                  <li>Monitoring</li>
-                  <li>Unexploded Ordinance</li>
-                </ul>
-              }
-            </td>
-          </tr>
-        </table>
+        <iframe title="DataExplorer" src="https://editor.p5js.org/lyuzixin@gmail.com/embed/80Ewhevrx"></iframe>
       </div>
     )
   }
